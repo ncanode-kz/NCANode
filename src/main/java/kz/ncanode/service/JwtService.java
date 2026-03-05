12dfa72a1c6d9275d18b0bfc69d1e09b1e9198c5
zhek <eudjin@gmail.com>
2026-03-05 17:03:12 +0500

    Prepare JWT structure, part 2

--- src/main/java/kz/ncanode/service/JwtService.java
+++ src/main/java/kz/ncanode/service/JwtService.java
@@ -4,6 +4,9 @@ import kz.ncanode.dto.request.JwtDecodeRequest;
 import kz.ncanode.dto.request.JwtEncodeRequest;
 import kz.ncanode.dto.response.JwtDecodeResponse;
 import kz.ncanode.dto.response.JwtEncodeResponse;
+import kz.ncanode.exception.ClientException;
+import kz.ncanode.exception.KeyException;
+import kz.ncanode.exception.ServerException;
 import kz.ncanode.wrapper.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,21 +40,33 @@ public class JwtService {
      */
     public JwtEncodeResponse encode(JwtEncodeRequest jwtEncodeRequest) {
 
-        String jwt = JWT.create().withIssuer("hello").withSubject("world")
-            .withClaim("key", "value")
-            .withIssuedAt(new java.util.Date())
-            .withExpiresAt(new java.util.Date(System.currentTimeMillis() + 60000))
-            .sign(Algorithm.GG2015(publicKey, privateKey));
+        try {
+            // read key
+            final KeyStoreWrapper keystore = kalkanWrapper.read(jwtEncodeRequest.getKey(), jwtEncodeRequest.getKeyAlias(), jwtEncodeRequest.getPassword());
+            final CertificateWrapper cert = keystore.getCertificate();
 
-        return JwtEncodeResponse.builder()
-            .jwt(jwt)
-            .build();
+            String jwt = JWT.create().withIssuer("hello").withSubject("world")
+                .withClaim("key", "value")
+                .withIssuedAt(new java.util.Date())
+                .withExpiresAt(new java.util.Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.GG2015((ECPublicKey) cert.getPublicKey(), (ECPrivateKey) keystore.getPrivateKey()));
 
+            return JwtEncodeResponse.builder()
+                .jwt(jwt)
+                .build();
 
+        } catch (KeyException e) {
+            throw new ClientException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServerException(e.getMessage(), e);
+        }
     }
 
     /**
      * Проверяет JWT-подписи
+     *
+     * @see https://gist.github.com/as1an/e87c371fc9fdece6a339d499f98e53ff
+     * @see https://dev.azure.com/as1an/public/_artifacts/feed/repo/maven/kz.gov.pki%2Fjava-jwt/overview/4.4.0
      *
      * @param jwt JWT-строка
      * @param key Ключ или сертификат
